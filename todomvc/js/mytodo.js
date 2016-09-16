d687c5dd790c894a0722efac1bc5569826d33b54
unknown <pp2moonbird@gmail.com>
2016-09-16 14:26:52 +0800

    add blur out

--- todomvc/js/mytodo.js
+++ todomvc/js/mytodo.js
@@ -43,6 +43,17 @@ var app = new Vue({
         editTodo: function(todo){
             this.beforeEditCache = todo.title;
             this.editedTodo = todo;
+        },
+
+        doneEdit: function(todo) {
+            if(!this.editedTodo){
+                return;
+            }
+            this.editedTodo = null;
+            todo.title = todo.title.trim();
+            if(!todo.title){
+                this.removeTodo(todo);
+            }
         }
 
 
