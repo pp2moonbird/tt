656a32b7e291a5f4284dba68ec6a9906641ff194
Peng <pp2moonbird@gmail.com>
2016-09-18 21:47:41 +0800

    3 patterns

--- todomvc/js/tt.js
+++ todomvc/js/tt.js
@@ -12,9 +12,21 @@ var app = new Vue({
             if(!value){
                 return;
             }
-            var pattern1 = /(\d{3,4}|now)-(\d{3,4}|now)/
+
+            var pattern1 = /(\d{3,4}|now)\s*-\s*(\d{3,4}|now)/
+            var pattern2 = /-\s*now/
+            var pattern3 = /now\s*-/
+
             if(pattern1.test(value)){
                 var result = pattern1.exec(value)
+                this.items.push({rawText:value, patternFound: result[0]});
+            }
+            else if(pattern2.test(value)){
+                var result = pattern2.exec(value)
+                this.items.push({rawText:value, patternFound: result[0]});
+            }
+            else if(pattern3.test(value)){
+                var result = pattern3.exec(value)
                 this.items.push({rawText:value, patternFound: result[0]});
             }
             else{
